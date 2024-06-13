4581173bf7fb3eabefaa45fb33085c886c37697c
Guy Barros <guy@Hashicorp.com>
2024-06-13 09:52:58 +0100

    updated actions

--- run.sh
+++ run.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
-
 npm install
 
 export HTTP_PORT=3000
 export ROOT_PATH=/chat
-
 export MONGODB_SERVER=localhost
 export MONGODB_PORT=27017
 export MONGODB_USERNAME=chat
 export MONGODB_PASSWORD=password
-
-
-export USE_HTTPS=true
-export PRIVATE_KEY_PATH=private-key.pem
-export PUBLIC_CERT_PATH=public-cert.pem
-export HTTPS_PORT=9443
-
-export USE_VAULT=true
-export VAULT_ADDR=http://localhost:8200
-export VAULT_TOKEN=""
-export VAULT_TRANSIT_MOUNT="transit"
-export VAULT_TRANSIT_KEY="chat-key"
-
-
-
+# export HTTP_TLS=true
+# export PRIVATE_KEY_PATH=private-key.pem
+# export PUBLIC_CERT_PATH=public-cert.pem
+# export HTTPS_PORT=9443
+# export USE_VAULT=true
+# export VAULT_ADDR=http://localhost:8200
+# export VAULT_TOKEN=""
+# export VAULT_TRANSIT_MOUNT="transit"
+# export VAULT_TRANSIT_KEY="chat-key"
 
 npm start
